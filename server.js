bc7ded561ca0a66bce244c36944c188d0253b225
shubhammorvekar <shubham992069@gmail.com>
2021-08-26 01:25:29 +0530

    made changes in public folder and create login reg page

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const ejsLayouts = require("express-ejs-layouts");
 const path = require("path");
 const port =process.env.PORT||3000;
 
-
-app.get("/", (req,res) => {
-    res.render("home");
-});
 app.use(express.static("public"));
 //setting ejs templates
 app.use(ejsLayouts);
 app.set('views', path.join(__dirname, "/resources/views"));
 app.set("view engine","ejs");
+
+app.get("/", (req,res) => {
+    res.render("home");
+});
+app.get("/register",(req,res) =>{
+    res.render("auth/register");
+});
+app.get("/login",(req,res) =>{
+    res.render("auth/login");
+});
+app.get("/cart", (req,res) => {
+    res.render("customers/cart");
+});
 
 // set server 
 app.listen(port, () => {
